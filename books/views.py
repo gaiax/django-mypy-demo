312a67031ebae26071aa3bdcdafa761d8d8c224e
chanyou0311 <chanyou0311@gmail.com>
2019-11-06 22:56:50 +0900

    [add] list

--- books/views.py
+++ books/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.db.models.query import QuerySet
+
+from .models import Book
+from typing import Dict
+
+ContextType = Dict[str, "QuerySet[Book]"]
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "books/index.html")
+
+
+def show_books(request: HttpRequest) -> HttpResponse:
+    books: "QuerySet[Book]" = Book.objects.all()
+    context: ContextType = {"books": books}
+    return render(request, "books/show.html", context)
